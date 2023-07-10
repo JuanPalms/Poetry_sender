09284094be968338542584802685650c93146261
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-07-09 18:08:56 -0600

    Adds attached image process

--- mail_sending/send_mail.py
+++ mail_sending/send_mail.py
@@ -4,6 +4,7 @@ This python module automates the process of mail sending
 import os
 import sys
 import yagmail
+import random
 
 
 # Add the parent directory to sys.path
@@ -20,13 +21,20 @@ loggin_keys = load_config('logging_key.yaml')
 
 sender = loggin_keys['logging_mail']
 
-receiver = loggin_keys['receiver']
 
 subject = "Daily poem"
 
-content = f'He is your daily poem: \n Title: {title} \n Author:{author} \n \n {poem} '
+number = random.randint(1,21)
+print(os.listdir('../images')[number])
+content = [f'Un poema para mi amor:\n \n Title: {title} \n Author:{author} \n \n {poem} ',
+          os.path.join('../images',os.listdir('../images')[number])]
 
 
 yag = yagmail.SMTP(user= sender, password=loggin_keys['app_password_mail'])
 
-yag.send(to=receiver, subject=subject, contents=content)
+#single receiver
+#receiver = loggin_keys['receiver']
+#yag.send(to=receiver, subject=subject, contents=content)
+
+for receiver in loggin_keys['receivers_list']:
+    yag.send(to=receiver, subject=subject, contents=content)
